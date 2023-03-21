54d59cdced8c77d97999366689f9dadace363768
Roberto Macias <robertomag7@hotmail.com>
2023-03-21 14:03:33 -0600

    Fixed images scroll effect on mobile devices

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 const images = document.querySelectorAll('.property__image');
 
 window.addEventListener('scroll', () => {
-    const scroll = this.scrollY / -20;
+    let scroll = this.scrollY / -20;
 
     images.forEach(image => {
+        if (scroll < -80) {
+            scroll += 80; 
+        }
+
         image.style.backgroundPositionY = `${scroll}px`;4
     });
 });
